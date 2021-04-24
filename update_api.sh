87731f8d8f68532679163480211e8f400c42dc52
Gobidev <adrian.groh@t-online.de>
2021-04-24 18:21:47 +0200

    Add update script

---
+++ update_api.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# check for running api
+check_screen=$(screen -ls | grep coriolis-api)
+
+if [ "$check_screen" != "" ]; then
+  echo "Running api found, terminating.."
+  screen -XS coriolis-api quit
+fi
+
+# update api
+./build.sh
+
+# start api
+screen -mdS coriolis-api node index.js
