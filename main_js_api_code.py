86dc9a3ce13f25cf1617e9c9601cecd7c5fd4418
Gobidev <adrian.groh@t-online.de>
2021-05-31 18:24:48 +0200

    Save urls of converted ship builds

--- main_js_api_code.py
+++ main_js_api_code.py
@@ -7,7 +7,8 @@ const zlib = require('zlib');
 const express = require("express");
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
-const app = express();"""
+const app = express();
+const fs = require('fs')"""
 
 main_api_js = """
 app.post("/convert", jsonParser, (req, res) => {
@@ -22,6 +23,32 @@ app.post("/convert", jsonParser, (req, res) => {
   // Get client information for log
   let logInformation = {"headers:": req.headers, "shipName": shipName}
   console.log(JSON.stringify(logInformation))
+
+  // Save converted build
+  buildToBeAdded = response.references[0].url
+  console.log(buildToBeAdded)
+
+  // Test if file exists
+  if (!fs.existsSync("conversions.json")) {
+    fs.writeFile("conversions.json", "[]", err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+    })
+  }
+  
+  fs.readFile("conversions.json", function (err, data) {
+    var json = JSON.parse(data)
+    json.push([+new Date(), buildToBeAdded])
+
+    fs.writeFile("conversions.json", JSON.stringify(json, null, 4), err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+    })
+  })
 
   res.type("json")
   res.send(response)
